c07c301583ead8550d8d061c09c9575aea988153
Andrew Koerner <andrew.koerner@dlr.de>
2026-05-26 12:52:19 +0200

    added sumo_test scenaro
    Signed-off-by: Andrew Koerner <andrew.koerner@dlr.de>

--- sumo_test.launch.py
+++ sumo_test.launch.py
@@ -51,7 +51,7 @@ def generate_launch_description():
             output='screen',
             parameters=[
                {"sumo_config_file": SUMO_CONFIG_PATH},
-               {"use_gui": True}
+               {"use_gui": True} ## set false to disable the sumo gui
             ],
         ),
     ])
